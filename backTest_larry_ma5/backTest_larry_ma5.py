fa95f3b3ee9ce4a88b2720cb0b82c15e9683d4d8
Youngseo Yoo <7dudtj@naver.com>
2021-07-24 17:40:51 +0900

    backTest_larry_ma5

---
+++ backTest_larry_ma5/backTest_larry_ma5.py
@@ -0,0 +1,19 @@
+import pybithumb
+import numpy as np
+
+df = pybithumb.get_ohlcv("BTC")
+k = 0.7
+
+df['ma5'] = df['close'].rolling(window = 5).mean().shift(1)
+df['range'] = (df['high'] - df['low']) * k
+df['target'] = df['open'] + df['range'].shift(1)
+df['bull'] = df['open'] > df['ma5']
+
+fee = 0.0032
+df['ror'] = np.where((df['high'] > df['target']) & df['bull'], df['close'] / df['target'] - fee, 1)
+
+df['hpr'] = df['ror'].cumprod()
+df['dd'] = (df['hpr'].cummax() - df['hpr']) / df['hpr'].cummax() * 100
+print("MMD:", df['dd'].max())
+print("HPR:", df['hpr'][-2])
+df.to_excel("larry_ma.xlsx")
